5cfac2577d25267a61d830d84545a71fc7dcca13
Danny Yu <dannyyu92@gmail.com>
2014-04-15 23:21:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PunditSampleApp::Application.config.secret_key_base = 'a90889a08af5e28454bbb95352fd33679f2e5fae6a3fe8de976c42dda913703f098f12183c7c4f0e8288b88f97fd33c59864d4085da12de6eee572fa0a9c80aa'
