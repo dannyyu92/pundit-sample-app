11204f595b4cd04011926a89311027d8b0d71aa5
Danny Yu <dannyyu92@gmail.com>
2014-04-17 23:28:59 -0400

    Add admin to secrets.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Default role set to user in migration
+  enum role: [:user, :admin]
 end
